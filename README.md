33424eb3575dc980a8979c91447db152da9f4292
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-16 21:21:42 +0700

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+
+/*
+//     {
+//     "workbench.iconTheme": "vscode-icons",
+//     "window.zoomLevel": 2,
+//     "files.exclude": {
+//       "**/.git": false,
+//         "**/.svn": true,
+//         "**/.hg": true,
+//         "**/.DS_Store": true
+//     },
+//     "editor.renderIndentGuides": true,
+//     "workbench.colorTheme": "Quiet Light",
+//     "vsicons.dontShowNewVersionMessage": true,
+//     "git.confirmSync": false,
+//     "editor.quickSuggestions": {
+//         "other": true,
+//         "comments": true,
+//         "strings": true
+//     },
+//     "[markdown]": {
+//         "editor.wordWrap": "on",
+//         "editor.quickSuggestions": true
+//     }
+// }
+*/
--- index.js
+++ index.js
@@ -8,7 +8,27 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 app.use(cookieParser());
-app.listen(3000, () => console.log('Server started'));
+app.listen(process.env.PORT || 3000, () => console.log('Server started'));
+
+const requireLogedIn = async (req, res, next) => {
+    try {
+        const obj = await getObject(req.cookies.token);
+        req.cookie = obj;
+        next();
+    } catch (e) {
+        res.send('Vui long dang nhap lai!!!');
+    }
+};
+
+const redirectIfLoggedIn = async (req, res, next) => {
+    try {
+        const obj = await getObject(req.cookies.token);
+        req.cookie = obj;
+        res.redirect('/private');
+    } catch (e) {
+        next();
+    }
+};
 
 app.get('/', (req, res) => {
     // res.cookie('AAA', '123');
@@ -16,32 +36,29 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/dangnhap', (req, res) => res.render('dangnhap'));
+app.get('/dangnhap', redirectIfLoggedIn, (req, res) => res.render('dangnhap'));
 
 app.post('/dangnhap', parser, async (req, res) => {
     const { username, password } = req.body;
-    const isExist = arrUser.some(e => e.username === username && e.password === password);
+    const isExist = arrUser.some(e => 
+        e.username === username && e.password === password);
     if (!isExist) return res.send('Kiem tra thong tin dang nhap');
     const token = await getToken({ username });
     res.cookie('token', token);
     res.send('Dang nhap thanh cong');
 });
 
-app.get('/private', async (req, res) => {
+app.get('/private', requireLogedIn, async (req, res) => {
     //kiem tra token => redirect neu chua dang nhap!!!
     //Xin chao + username
-    try {
-        const obj = await getObject(req.cookies.token);
-        res.send(`Xin chao ${obj.username}`);
-    } catch (e) {
-        res.send('Vui long dang nhap lai!!!');
-    }
+    res.send('Xin chao ' + req.cookie.username);
 });
+
+
 
 const arrUser = [
     { username: 'teo', password: '123' },
     { username: 'ti', password: '234' },
     { username: 'tun', password: '567' },
 ];
-
 
