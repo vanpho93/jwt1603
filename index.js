24397a10694a17474e23443da3d9d96e6d4c81ef
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-16 20:51:19 +0700

    xong

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const parser = require('body-parser').urlencoded({ extended: false });
+const { getToken, getObject } = require('./token');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -16,13 +18,24 @@ app.get('/', (req, res) => {
 
 app.get('/dangnhap', (req, res) => res.render('dangnhap'));
 
-app.post('/dangnhap', (req, res) => {
-
+app.post('/dangnhap', parser, async (req, res) => {
+    const { username, password } = req.body;
+    const isExist = arrUser.some(e => e.username === username && e.password === password);
+    if (!isExist) return res.send('Kiem tra thong tin dang nhap');
+    const token = await getToken({ username });
+    res.cookie('token', token);
+    res.send('Dang nhap thanh cong');
 });
 
-app.get('/private', (req, res) => {
+app.get('/private', async (req, res) => {
     //kiem tra token => redirect neu chua dang nhap!!!
     //Xin chao + username
+    try {
+        const obj = await getObject(req.cookies.token);
+        res.send(`Xin chao ${obj.username}`);
+    } catch (e) {
+        res.send('Vui long dang nhap lai!!!');
+    }
 });
 
 const arrUser = [
